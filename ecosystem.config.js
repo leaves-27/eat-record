bde382b9e7a9429b4d76f36dd95a1f3a3862e9f3
leaves-27 <1132761957@qq.com>
2017-02-26 07:37:10 +0800

    添加部署文件配置

---
+++ ecosystem.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+
+    // First application
+    {
+      name      : "eat-record",
+      script    : "./build/eat-record/server.js",
+      env: {
+        COMMON_VARIABLE: "true"
+      },
+      env_production : {
+        NODE_ENV: "production"
+      }
+    }
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy : {
+    production : {
+      user : "leaves-27",
+      host : "funnyxiu.com",
+      ref  : "origin/0225",
+      repo : "https://github.com/leaves-27/eat-record.git",
+      path : "/data/nodejs/eat-record",
+      "post-deploy" : "npm install && bower install && npm run prod && pm2 restart ./build/eat-record/server.js"
+    }
+  }
+}
