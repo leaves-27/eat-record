4a96475dd2a617664304d015a6d958119f88250c
dong.keke <dong.keke@tongbanjie.com>
2017-04-17 00:07:10 +0800

    依据路由，分开打包

--- src/client/routes/index.js
+++ src/client/routes/index.js
@@ -10,43 +10,43 @@ import Login from '../../common/containers/login';
 import Detail from '../../common/containers/detail';
 import Backend from '../../common/containers/backend';
 
-const routeConfig = [{ 
-  path: 'web',
-  component: App,
-  indexRoute: { 
-    component: List 
-  },
-  childRoutes: [{ 
-    path: 'login', 
-    component: Login 
-  },
-  { 
-    path: 'detail/:date',
-    component: Detail
-  },
-  { 
-    path: 'backend',
-    component: Backend
-  }]
-}]
-
-// const routes = {
+// const routeConfig = [{ 
 //   path: 'web',
-//   indexRoute:{
-//     getComponent(nextState, cb) {
-//       require.ensure([], (require) => {
-//         cb(null, require('../../common/containers/list').default)
-//       })
-//     }
+//   component: App,
+//   indexRoute: { 
+//     component: List 
 //   },
-//   component: require('../../common/containers/app').default,
-//   childRoutes: [
-//     require('./detail'),
-//     require('./backend'),
-//     require('./login'),
-//     require('./404')
-//   ]
-// }
+//   childRoutes: [{ 
+//     path: 'login', 
+//     component: Login 
+//   },
+//   { 
+//     path: 'detail/:date',
+//     component: Detail
+//   },
+//   { 
+//     path: 'backend',
+//     component: Backend
+//   }]
+// }]
+
+const routeConfig = {
+  path: 'web',
+  indexRoute:{
+    getComponent(nextState, cb) {
+      require.ensure([], (require) => {
+        cb(null, require('../../common/containers/list').default)
+      })
+    }
+  },
+  component: require('../../common/containers/app').default,
+  childRoutes: [
+    require('./detail'),
+    require('./backend'),
+    require('./login'),
+    require('./404')
+  ]
+}
 
 export default (history) => {
   return (
