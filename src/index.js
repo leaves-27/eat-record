106b4b1b0f308c25f11f115b91c92ed5e4bf6588
dong.keke <dong.keke@tongbanjie.com>
2017-07-07 12:27:37 +0800

    11

--- src/index.js
+++ src/index.js
@@ -29,41 +29,41 @@ const store = middlewareConfig(about,window.__REDUX_DEVTOOLS_EXTENSION__ && wind
   ** 未登录的情况下，返回值code为4。
   **/
 
-// store.subscribe(function(){
-//   const state = store.getState();
-//   const location = state.routing.locationBeforeTransitions;
+store.subscribe(function(){
+  const state = store.getState();
+  const location = state.routing.locationBeforeTransitions;
 
-//   if(state.login.status==1) {
-//     if(location.pathname=="/login"){
-//       let redirectUrl = "/backend";
+  if(state.login.status==1) {
+    if(location.pathname=="/login"){
+      let redirectUrl = "/backend";
 
-//       if(location.query && location.query.redirectUrl){
-//         redirectUrl = location.query.redirectUrl;
-//       }
+      if(location.query && location.query.redirectUrl){
+        redirectUrl = location.query.redirectUrl;
+      }
 
-//       browserHistory.push({
-//         pathname:redirectUrl
-//       });
-//     }
-//   }else{
-//     if(location.pathname!="/login" && location.pathname!="/register"){
-//       let redirectUrl = "/login";
-//       browserHistory.push({
-//         pathname : redirectUrl,
-//         query:{
-//           redirectUrl : location.pathname
-//         }
-//       });
-//     }else{
-//       if(state.register && state.register.status && state.register.status==1) {
-//         let redirectUrl = "/login";
-//         browserHistory.push({
-//           pathname:redirectUrl
-//         });
-//       }
-//     }
-//   }
-// });
+      browserHistory.push({
+        pathname:redirectUrl
+      });
+    }
+  }else{
+    if(location.pathname!="/login" && location.pathname!="/register"){
+      let redirectUrl = "/login";
+      browserHistory.push({
+        pathname : redirectUrl,
+        query:{
+          redirectUrl : location.pathname
+        }
+      });
+    }else{
+      if(state.register && state.register.status && state.register.status==1) {
+        let redirectUrl = "/login";
+        browserHistory.push({
+          pathname:redirectUrl
+        });
+      }
+    }
+  }
+});
 
 const history = syncHistoryWithStore(browserHistory,store);
 render(
